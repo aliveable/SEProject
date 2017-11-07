f4a03b290ac9ee2d9259b68bb2fd362d4ab6115e
58070012 <58070012@kmitl.ac.th>
2017-11-07 20:08:38 +0700

    Update js(jquery)

--- SEProject/WebpagePrototype/Sample/JS/Login.js
+++ SEProject/WebpagePrototype/Sample/JS/Login.js
@@ -1,8 +1,5 @@
-$(document).ready(function() {
-  // Smooth scrolling
-   $("#scroll").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#header").offset().top-50
-    }, 1000 );
-});
+$("#scroll").click(function() {
+    $('html, body, header').animate({
+        scrollTop: $("#header").offset().top
+    }, 1500 );
 });
