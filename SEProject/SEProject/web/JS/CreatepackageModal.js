3d265732a337965afe49cb02265fed9e9c1962be
Tanawat Tiangtam <littlejab@windowslive.com>
2017-12-10 17:09:14 +0700

    Add CreatePackageModal JS

---
+++ SEProject/SEProject/web/JS/CreatepackageModal.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(document).ready(function () {
+    $('#edititemOptional1').click(function (){
+        $('#editmodaloptional').modal('show');
+        
+    });
+    
+    
+    $('#additemoptional').click(function (){
+        $('#additemoptionalmodel').modal('show');
+        
+    });
+    
+     $('#edititem2').click(function (){
+        $('#editmodalinclude').modal('show');
+        
+    });
+    
+    $('#additemincludebutton').click(function (){
+        $('#additemincludemodel').modal('show');
+        
+    });
+    
+});
+
+
