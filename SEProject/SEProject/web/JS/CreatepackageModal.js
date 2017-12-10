2dc530b1d759c9266b49cd23d4c40ae3fc1a13f8
aliveable <aliveable1754@gmail.com>
2017-12-11 02:30:40 +0700

    add display

--- SEProject/SEProject/web/JS/CreatepackageModal.js
+++ SEProject/SEProject/web/JS/CreatepackageModal.js
@@ -3,28 +3,76 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var keepInclude = [];
+var keepOptional = [];
+
 $(document).ready(function () {
-    $('#edititemOptional1').click(function (){
+    $('#edititemOptional').on("click", function () {
         $('#editmodaloptional').modal('show');
-        
     });
-    
-    
-    $('#additemoptional').click(function (){
+
+
+    $('#additemoptional').click(function () {
         $('#additemoptionalmodel').modal('show');
-        
+
     });
-    
-     $('#edititem2').click(function (){
+
+    $('#edititem2').click(function () {
         $('#editmodalinclude').modal('show');
-        
+
     });
-    
-    $('#additemincludebutton').click(function (){
+
+    $('#additemincludebutton').click(function () {
         $('#additemincludemodel').modal('show');
-        
+
+    });
+
+    $('.addinc').on('click', '#edititem2',function(){
+//        alert($(this).closest('button').index());
+    
     });
     
 });
+
+function edittiem(ind){
+    alert("fuck");
+    $('#recipient-name-include-add').val(keepInclude[ind]);
+}
+
+function addItem() {
+
+    $('#addItemContainer.row:last').after('<div class="row">' +
+            '<div class="col-md-11" id="addcomplete" style="">' +
+            '<ul style="font-size: 1.8em;"><div style="word-wrap: break-word">' + $('#recipient-name-include-add').val() + '</div></ul>' +
+            '</div>' +
+            '<div class="col-md-1">' +
+            '<button id="edititem2" type="button" class="btn btn-success" style="margin-left: 2%"> Edit</button>' +
+            '</div>' +
+            '</div>');
+    keepInclude.push($('#recipient-name-include-add').val());
+}
+
+function editOptionalItem(){
+    $('#recipient-name').val(keepOptional[0][0]);
+    $('#recipient-price').val(keepOptional[0][1]);
+    $('#recipient-max').val(keepOptional[0][2])
+    
+}
+function addItemOptional() { 
+    $('#addItemContainerOptional.row:last').after('<div class="row">' +
+            '<div class="col-md-11" style="">' +
+            '<ul style="font-size: 1.8em;"> <div style="word-wrap: break-word">' + 'Optional: ' + $('#recipient-name').val() +
+            ' Price: ' + $('#recipient-price').val() + ' Max: ' + $('#recipient-max').val() + '</div> </ul>' +
+            '</div>' +
+            '<div class="col-md-1" style="float: right">' +
+            '<button id="edititemOptional1" type="button" class="btn btn-success" style="margin-left: 2%;float: right"> Edit</button>' +
+            '</div>' +
+            '</div>');
+    keepOptional.push([$('#recipient-name').val(), $('#recipient-price').val(), $('#recipient-max').val()]);
+}
+
+
+
+
 
 
