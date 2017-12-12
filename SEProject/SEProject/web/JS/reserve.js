7acc3a094c90c106a6d7123f2ebb9e06738e9819
XenXorX <it58070024@office365.it.kmitl.ac.th>
2017-12-12 19:32:08 +0700

    Removable reserve item

--- SEProject/SEProject/web/JS/reserve.js
+++ SEProject/SEProject/web/JS/reserve.js
@@ -2,10 +2,9 @@ $(document).ready(function () {
     $('#addpackagecreate').click(function () {
         addReserve();
     });
-
-    $('#removepackage').on('click', '#removepackage', function () {
-        alert("click");
-        //$(this).parent().parent().html();
+    
+    $('#reserve-list').on('click', '#removepackage', function() {
+        $(this).parent().parent().remove();
     });
 });
 
@@ -18,7 +17,7 @@ function addReserve() {
             '   <label style="font-size: 1.5em">' + $('#input-people').val() + '</label> <label style="font-size: 1.6em">People</label>\n' +
             '</div>\n' +
             '<div class="col-lg-4">\n' +
-            '   <button  id="removepackage" type="button" class="btn btn-danger mr-right" style="float: right">Remove</button>\n' +
+            '   <button id="removepackage" type="button" class="btn btn-danger mr-right" style="float: right">Remove</button>\n' +
             '</div>\n';
 
     $('#reserve-list').append(html);
